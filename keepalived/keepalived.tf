d1628c095bada88ab598bad1c630254943a2df01
Anatoliy <anatoliykv@gmail.com>
2020-01-12 21:53:48 +0200

    Update keepalived.tf

--- keepalived/keepalived.tf
+++ keepalived/keepalived.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "mywebserver" {
     ami                    = "ami-0cc0a36f626a4fdf5" #Ubuntu
     instance_type          = "t2.micro"
     vpc_security_group_ids = [aws_security_group.my_webserver.id]
+    key_name = "key for ubuntu"
     user_data = <<EOF
 #!/bin/bash
 sudo apt-get update
@@ -49,12 +50,7 @@ resource "aws_security_group" "my_webserver" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]# add a CIDR block here
   }
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]# add a CIDR block here
-  }
+
   egress {
     from_port       = 0
     to_port         = 0
@@ -66,8 +62,4 @@ ingress {
     Name = "WebServer Security Group Terraform"
     Owner = "anatoliykv Terraform"
   }
-}
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQe+epk9i20YD3B2XCJjNH8sGUi5sfIHvvaZNZ4Mncxt0jSRrYNoLsZklyU5gs6pUdoB2e1/73tqq7Jn7N/kLoeTUVq8ZUneItbDi9C53c1W2Jpd237hvh4n0TfpnZfJOhExyubFvQjPPnsBOhIdUbrolieIXsvhSwHxNyJcZpZfehMYt7vLZ6NarUweYqAwjssFnKhtWmhdwCXiyxXkT7aaHJQ4tWK/YExNELNfP5azCJB40SY5JAgzCDaQInchj29xeY4a/yfioD6usvlHtD2FQtoEsBvao/l3tbBtaBP8yQgZrSdNcQNictF6O4M72iyjbVru6Q8NEu4aK+s4ef ubuntu@ip-172-31-32-159"
 }
