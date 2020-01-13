e06d2a73d8386f6f6cac689077af58855b837508
Anatoliy <anatoliykv@gmail.com>
2020-01-13 11:30:22 +0200

    Update keepalived.tf

--- keepalived/keepalived.tf
+++ keepalived/keepalived.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "my_webserver" {
   }
 
   ingress {
-    from_port   = 82
-    to_port     = 82
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]# add a CIDR block here
   }
